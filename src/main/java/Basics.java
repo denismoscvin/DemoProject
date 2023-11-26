dd3218b4b74b19a5735a85e1049fdd9ab7deeb52
denismoscvin <moscvindenis95@gmail.com>
2023-11-26 18:04:19 +0200

    import static given

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -1,25 +1,28 @@
-import files.payload;
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
 
-import io.restassured.RestAssured;
-import static io.restassured.RestAssured.*;
-
+import static io.restassured.RestAssured.given;
 
 public class Basics {
     public static void main(String[] args) {
         RestAssured.baseURI = "https://rahulshettyacademy.com";
-//        String response = given().log().all().header("Content-Type", "application/json")
-//                .body(payload.AddPlace()).when().post("api/en/account/login")
-//                .then().statusCode(200).body("email", equalTo("denis.m@gbl-factory.com"))
-//                .extract().response().asString();
-//
-//        System.out.println(response);
-//        System.out.println();
-//
-//
-//        JsonPath jsonPath = new JsonPath(response);
-//        String emailUser = jsonPath.getString("email");
+        String response = given().log().all().header("Content-Type", "application/json")
+                .body("{\n" +
+                        "    \"location\": {\n" +
+                        "        \"lat\": -99.383494,\n" +
+                        "        \"lng\": 99.427362\n" +
+                        "    },\n" +
+                        "    \"accuracy\": 50,\n" +
+                        "    \"name\": \"Soroca house\",\n" +
+                        "    \"phone_number\": \"(+91) 983 893 3999\",\n" +
+                        "    \"address\": \"99, side layout, cohen 99\",\n" +
+                        "    \"types\": [\n" +
+                        "        \"shoe park\",\n" +
+                        "        \"shop\"\n" +
+                        "    ],\n" +
+                        "    \"website\": \"http://google.com\",\n" +
+                        "    \"language\": \"English-IN\"\n" +
+                        "}").when().post("/maps/api/place/add/json")
+                .then().assertThat()
     }
 }
 
