d4a1906755e9808bcb9ae930b2a98d811935dccf
denismoscvin <moscvindenis95@gmail.com>
2023-11-26 17:45:35 +0200

    import clases

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -2,13 +2,13 @@ import files.payload;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 
 
 public class Basics {
     public static void main(String[] args) {
-//        RestAssured.baseURI = "https://crm-dev.gbl-systems.com";
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
 //        String response = given().log().all().header("Content-Type", "application/json")
 //                .body(payload.AddPlace()).when().post("api/en/account/login")
 //                .then().statusCode(200).body("email", equalTo("denis.m@gbl-factory.com"))
@@ -18,8 +18,8 @@ public class Basics {
 //        System.out.println();
 //
 //
-        JsonPath jsonPath = new JsonPath(response);
-        String emailUser = jsonPath.getString("email");
+//        JsonPath jsonPath = new JsonPath(response);
+//        String emailUser = jsonPath.getString("email");
     }
 }
 
