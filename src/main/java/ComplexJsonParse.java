2c96f1b86c02fba7e3fa1d5767bba359c91683a7
denismoscvin <moscvindenis95@gmail.com>
2023-12-10 21:03:06 +0200

    Lectia 31 am terminat

---
+++ src/main/java/ComplexJsonParse.java
@@ -0,0 +1,55 @@
+import files.payload;
+import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
+
+public class ComplexJsonParse {
+    public static void main(String[] args) {
+        JsonPath jsonPath = new JsonPath(payload.CoursePrice());
+
+        //Print no of courses returned by API
+        int count = jsonPath.getInt("courses.size()");
+        System.out.println(count);
+
+        //Print purchased amount
+        int purchasedAmount = jsonPath.getInt("dashboard.purchaseAmount");
+        System.out.println(purchasedAmount);
+
+        //Print title of the first course
+        String nameOfFirstCourse = jsonPath.getString("courses[0].title");
+        System.out.println(nameOfFirstCourse);
+
+        //Print all courses title and respective price
+        for (int i = 0; i < count; i++) {
+            String courseTitles = jsonPath.getString("courses[" + i + "].title");
+            int coursePrice = jsonPath.getInt("courses[" + i + "].price");
+            System.out.println(courseTitles + ": " + coursePrice);
+        }
+
+        //Print no of copies sold by RPA courses
+        for (int i = 0; i < count; i++) {
+            String courseTitles = jsonPath.getString("courses[" + i + "].title");
+            if (courseTitles.equalsIgnoreCase("RPA")) {
+                int copies = jsonPath.getInt("courses[" + i + "].copies");
+                System.out.println("no of copies sold by RPA courses: " + copies);
+                break;
+            }
+
+        }
+
+        //Print if Sum of all courses matches with Purchased Amount
+        int sum = 0;
+        for (int i = 0; i < count; i++) {
+            int coursePrice = jsonPath.getInt("courses[" + i + "].price");
+            int courseCount = jsonPath.getInt("courses[" + i + "].copies");
+            sum = sum + (coursePrice * courseCount);
+        }
+        System.out.println(sum);
+
+        Assert.assertEquals(sum, purchasedAmount);
+//        if (sum == purchasedAmount) {
+//            System.out.println("The sum of courses and purchased Amount is matched");
+//        } else {
+//            System.out.println("The sum of courses and purchased Amount is matched");
+//        }
+    }
+}
--- src/main/java/files/payload.java
+++ src/main/java/files/payload.java
@@ -2,7 +2,7 @@ package files;
 
 public class payload {
     public static String Addplace() {
-        String payload ="{\n" +
+        String payload = "{\n" +
                 "    \"location\": {\n" +
                 "        \"lat\": -99.383494,\n" +
                 "        \"lng\": 99.427362\n" +
@@ -19,5 +19,36 @@ public class payload {
                 "    \"language\": \"English-IN\"\n" +
                 "}";
         return payload;
+    }
+
+    public static String CoursePrice() {
+        return "{\n" +
+                "  \"dashboard\": {\n" +
+                "    \"purchaseAmount\": 1162,\n" +
+                "    \"website\": \"https://rahulshettyacademy.com\"\n" +
+                "  },\n" +
+                "  \"courses\": [\n" +
+                "    {\n" +
+                "      \"title\": \"Selenium Python\",\n" +
+                "      \"price\": 50,\n" +
+                "      \"copies\": 6\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"title\": \"Cypress\",\n" +
+                "      \"price\": 40,\n" +
+                "      \"copies\": 4\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"title\": \"RPA\",\n" +
+                "      \"price\": 45,\n" +
+                "      \"copies\": 10\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"title\": \"Appium\",\n" +
+                "      \"price\": 36,\n" +
+                "      \"copies\": 7\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
     }
 }
